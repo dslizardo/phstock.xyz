6a1bd2ffc8fa54da81dcb86e6d1e95a1e869b2a1
Daniel Lizardo <dslizardo@gmail.com>
2018-02-19 23:05:34 +0800

    Add endpoint for top gainers

--- app.py
+++ app.py
@@ -7,14 +7,16 @@ redis = redis_wrapper.redis_client
 
 @app.route("/stocks")
 def get_all_stocks():
-    stocks=redis.get('stocks:all')
-    return stocks
+    return redis.get('stocks:all')
 
 @app.route("/stock/<code>")
 def get_stock(code):
-    stock=redis.get('stocks:'+code)
-    return stock
+    return redis.get('stocks:'+code)
+
+@app.route("/stocks/top-gainers")
+def get_top_gainers():
+    return redis.get('stocks:top_gainers')
 
 if __name__ == "__main__":
-    ticker.get_stocks()
+    ticker.store_stocks()
     app.run()
--- ticker.py
+++ ticker.py
@@ -3,6 +3,7 @@ import threading
 import json
 import datetime
 import redis_wrapper
+import decimal
 
 
 HOST = 'http://www.pse.com.ph/stockMarket/home.html'
@@ -10,11 +11,17 @@ HEADERS = {'Referer': HOST}
 
 redis = redis_wrapper.redis_client
 
-def get_stocks():
-    threading.Timer(10.0, get_stocks).start()
+def store_stocks():
+    threading.Timer(10.0, store_stocks).start()
     r = requests.get(HOST+'?method=getSecuritiesAndIndicesForPublic&ajax=true', headers=HEADERS)
     stocks = r.json()
-
     for stock in stocks:
-        redis.set('stocks:'+stock['securitySymbol'], stock)
-    redis.set('stocks:all', stocks)
+        redis.set('stocks:'+stock['securitySymbol'], json.dumps(stock))
+    redis.set('stocks:all', json.dumps(stocks))
+    top_gainers=get_top_gainers(json.dumps(stocks))
+    redis.set('stocks:top_gainers', top_gainers)
+
+def get_top_gainers(json_data):
+    data=json.loads(json_data)
+    sorted_data=sorted(data[1:], key=lambda x : decimal.Decimal(x['percChangeClose']), reverse=True)
+    return sorted_data[:10]
