f3cf6fa6bed1c884ee03010cc9ab4fd74f0aec1b
Daniel Lizardo <dslizardo@gmail.com>
2018-02-26 22:45:19 +0800

    Check if redis is running before starting the application

--- run.sh
+++ run.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+echo 'Check if redis is running'
+check=`redis-cli ping`
+
+if [[ ${check} == "PONG" ]];
+then
+  echo "Redis is running"
+else
+  echo "Redis is not running"
+  echo "Starting redis..."
+  start_redis=`redis-server --daemonize yes`
+  if [[ $1 -eq 1 ]];
+  then
+     echo "Done"
+  fi
+fi
+
 export FLASK_APP=run.py
 export FLASK_DEBUG=1
 
