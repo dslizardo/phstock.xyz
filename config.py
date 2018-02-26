60bba20e92b181887afdf726938f93d61d1134b4
Daniel Lizardo <dslizardo@gmail.com>
2018-02-26 22:01:55 +0800

    Add smtp configuration

--- config.py
+++ config.py
@@ -1,2 +1,11 @@
-redis = {'host': 'localhost',
-         'port': 6379}
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+DEBUG = True # For development purposes only
+
+# Flask Mail Configuration
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 465
+MAIL_USERNAME = 'test@example'
+MAIL_PASSWORD = 'password'
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
